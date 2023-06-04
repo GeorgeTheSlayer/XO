d94230af67477ca808ead0cd5e7375cf46aabbc3
GeorgeTheSlayer <holland@sersen.net>
2023-06-04 11:18:33 -0500

    Final Version this time

--- src/App.svelte
+++ src/App.svelte
@@ -37,6 +37,7 @@
   let gain: number = 50;
   let counter = 0;
   let isRunning = false;
+  let keyVal = false;
   let xClass = "text-black";
   let oClass = "text-black";
 
@@ -104,7 +105,6 @@
       } else {
         params[index].value = params[index].value + 1;
       }
-      console.log(params[index].value);
     }
   }
 
@@ -125,6 +125,20 @@
     } else {
       return false;
     }
+  }
+
+  function resetParams() {
+    params.forEach((param) => {
+      param.value = 0;
+    });
+    keyVal = !keyVal;
+  }
+
+  function randomizeParams() {
+    params.forEach((param) => {
+      param.value = Math.floor(Math.random() * 3);
+    });
+    keyVal = !keyVal;
   }
 
   $: {
@@ -161,96 +175,151 @@
           <span class={oClass}>0</span>
         </h1>
         <p class="text-xl opacity-50">Press Play and Tap the Pads</p>
-        <div class="square grid grid-cols-4 max-w-lg p-2 mx-auto gap-4 mt-4">
-          {#each params as pam, index}
-            <button
-              class="square {counter == index
-                ? 'bg-black text-white'
-                : 'bg-slate-200 text-black'} font-bold text-4xl lg:text-7xl text-center p-4 hover:opacity-50"
-              on:click={() => changeParam(index)}
+        {#key keyVal}
+          <div class="square grid grid-cols-4 max-w-lg p-2 mx-auto gap-4 mt-4">
+            {#each params as pam, index}
+              <button
+                class="square {counter == index
+                  ? 'bg-black text-white'
+                  : 'bg-slate-200 text-black'} font-bold text-4xl lg:text-7xl text-center p-4 hover:opacity-70"
+                on:click={() => changeParam(index)}
+              >
+                {pamToDisp(pam.value)}
+              </button>
+            {/each}
+          </div>
+        {/key}
+        <div class="flex justify-evenly max-w-xl mx-auto w-full">
+          <!-- randomize -->
+          <button
+            on:click={randomizeParams}
+            class="text-white ease-in-out duration-200 hover:opacity-50 bg-black p-4 mt-2 max-w-lg"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-dice-5-filled"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
             >
-              {pamToDisp(pam.value)}
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M18.333 2c1.96 0 3.56 1.537 3.662 3.472l.005 .195v12.666c0 1.96 -1.537 3.56 -3.472 3.662l-.195 .005h-12.666a3.667 3.667 0 0 1 -3.662 -3.472l-.005 -.195v-12.666c0 -1.96 1.537 -3.56 3.472 -3.662l.195 -.005h12.666zm-2.833 12a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0 -3zm-7 0a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0 -3zm3.5 -3.5a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0 -3zm-3.5 -3.5a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0 -3zm7 0a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0 -3z"
+                stroke-width="0"
+                fill="currentColor"
+              />
+            </svg></button
+          >
+          <!-- Start -->
+          {#if !isRunning}
+            <button
+              class="text-white ease-in-out duration-200 hover:opacity-50 bg-black p-4 mt-2 max-w-lg"
+              on:click={startAudio}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon icon-tabler icon-tabler-player-play-filled"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path
+                  d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z"
+                  stroke-width="0"
+                  fill="currentColor"
+                />
+              </svg>
             </button>
-          {/each}
+          {:else}
+            <button
+              class="text-white ease-in-out duration-200 hover:opacity-50 bg-black p-4 mt-2 max-w-lg"
+              on:click={startAudio}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon icon-tabler icon-tabler-player-pause-filled"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path
+                  d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z"
+                  stroke-width="0"
+                  fill="currentColor"
+                />
+                <path
+                  d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z"
+                  stroke-width="0"
+                  fill="currentColor"
+                />
+              </svg>
+            </button>
+          {/if}
+          <!-- reset -->
+          <button
+            on:click={resetParams}
+            class="text-white ease-in-out duration-200 hover:opacity-50 bg-black p-4 mt-2 max-w-lg"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-clear-all"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M8 6h12" />
+              <path d="M6 12h12" />
+              <path d="M4 18h12" />
+            </svg></button
+          >
         </div>
-        <div class="flex mx-auto w-full max-w-lg justify-evenly">
-          <div class="flex flex-col">
+        <div
+          class="flex mx-auto mt-4 w-full max-w-lg p-4 gap-x-8 justify-evenly"
+        >
+          <div class="flex flex-col w-full">
             <input
               type="range"
               name="tempo"
+              class="slider"
               max="160"
               min="40"
               bind:value={tempo}
             />
-            <label for="tempo">Tempo {tempo}</label>
+            <label class="text-lg" for="tempo">Tempo: {tempo}</label>
           </div>
-          <div class="flex flex-col">
+          <div class="flex flex-col w-full">
             <input
+              class="slider"
               type="range"
               name="gainVal"
               max="100"
               min="0"
               bind:value={gain}
             />
-            <label for="gainVal">Gain: {gain}</label>
+            <label class="text-lg" for="gainVal">Gain: {gain}</label>
           </div>
         </div>
-        {#if !isRunning}
-          <button
-            class="text-white bg-black p-4 mt-2 max-w-lg"
-            on:click={startAudio}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-player-play-filled"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path
-                d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z"
-                stroke-width="0"
-                fill="currentColor"
-              />
-            </svg>
-          </button>
-        {:else}
-          <button
-            class="text-white bg-black p-4 mt-2 max-w-lg"
-            on:click={startAudio}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-player-pause-filled"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path
-                d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z"
-                stroke-width="0"
-                fill="currentColor"
-              />
-              <path
-                d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z"
-                stroke-width="0"
-                fill="currentColor"
-              />
-            </svg>
-          </button>
-        {/if}
       {/await}
     </div>
     <h1
@@ -260,3 +329,40 @@
     </h1>
   </body>
 </main>
+
+<style lang="postcss">
+  /* The slider itself */
+  .slider {
+    -webkit-appearance: none; /* Override default CSS styles */
+    appearance: none;
+    width: 100%; /* Full-width */
+    height: 25px; /* Specified height */
+    background: #838383; /* Grey background */
+    outline: none; /* Remove outline */
+    opacity: 0.7; /* Set transparency (for mouse-over effects on hover) */
+    -webkit-transition: 0.2s; /* 0.2 seconds transition on hover */
+    transition: opacity 0.2s;
+  }
+
+  /* Mouse-over effects */
+  .slider:hover {
+    opacity: 1; /* Fully shown on mouse-over */
+  }
+
+  /* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
+  .slider::-webkit-slider-thumb {
+    -webkit-appearance: none; /* Override default look */
+    appearance: none;
+    width: 25px; /* Set a specific slider handle width */
+    height: 25px; /* Slider handle height */
+    background: black; /* Green background */
+    cursor: pointer; /* Cursor on hover */
+  }
+
+  .slider::-moz-range-thumb {
+    width: 25px; /* Set a specific slider handle width */
+    height: 25px; /* Slider handle height */
+    background: black; /* Green background */
+    cursor: pointer; /* Cursor on hover */
+  }
+</style>
